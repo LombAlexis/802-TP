924a0aeb184a7af902014547720dec3218645bc6
fendi74960 <serpiente61@hotmail.fr>
2022-01-20 11:27:17 +0100

    Ajout des donnes votures

--- api-saop/car.py
+++ api-saop/car.py
@@ -1,7 +1,12 @@
 class Car:
-    def __init__(self, name, autonomy):
+    def __init__(self, name, autonomy, topSpeed, efficiency, fastCharge, normalCharge, capacity):
         self.name = name
         self.autonomy = autonomy
+        self.topSpeed = topSpeed
+        self.efficiency = efficiency
+        self.fastCharge = fastCharge
+        self.normalCharge = normalCharge
+        self.capacity = capacity
 
     def __str__(self):
      return self.name + " " + self.autonomy
@@ -17,6 +22,9 @@ class Car:
     @staticmethod
     def loadCars():
         cars = []
-        cars.append(Car("Voiture", 400))
+        cars.append(Car("Tesla Model 3",380,225,150,170000,11000,57000))
+        cars.append(Car("Dacia Spring Electric",170,125,158,34000,6600,26800))
+        cars.append(Car("Kia EV6 GT",395,260,196,233000,11000,77400))
+        cars.append(Car("BMW i4 eDrive40",475,190,170,200000,11000,80700))
         
         return cars;
