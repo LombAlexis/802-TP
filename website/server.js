fe1cff9e531f2c6401d9257aadf6e4151a7384ef
fendi74960 <serpiente61@hotmail.fr>
2022-02-04 16:46:11 +0100

    api pour les bornes faite, tester et apparait sur la carte
    par contre pas bonne icone et coords en brute pour l'instant

--- website/server.js
+++ website/server.js
@@ -3,6 +3,9 @@ var app = express();
 var http = require('http').Server(app);
 var soap = require('soap');
 var io = require('socket.io')(http);
+var fetch = require('node-fetch');
+
+
 
 var url = 'https://calm-basin-70231.herokuapp.com/?wsdl';
 
@@ -33,10 +36,41 @@ function carInfo(io, name) {
 	});
 }
 
+async function borneDemande(io,longitude,lattitude){
+	const response = await fetch('https://opendata.reseaux-energies.fr/api/records/1.0/search/?dataset=bornes-irve&rows=50&geofilter.distance='+ longitude +','+ lattitude +',1000')
+	.catch(erreur => { throw erreur})
+  	const myJson = await response.json()
+
+	//traitement json
+	let myData = [[],[]];
+	for (let i=0; i< myJson['nhits']; i++){
+		myData[0].push(myJson['records'][i]['fields']['xlongitude'])
+		myData[1].push(myJson['records'][i]['fields']['ylatitude'])
+	}
+
+	let longi = 0
+	let lati = 0
+	let moy = (longitude + lattitude) / 2
+
+	for (let i=0; i< myJson['nhits']; i++){
+		if (moy > (myData[0][i] + myData[1][i])/2){
+			moy = ((myData[0][i] + myData[1][i])/2);
+			longi = myData[0][i]
+			lati = myData[1][i]
+		}
+	}
+	io.emit('borneTrouve', [longi,lati]);
+}
+
 io.on('connect', function(socket){
 	carsList(io);
 
 	socket.on('car', function (name) {
 		carInfo(io, name);
 	});
+
+	socket.on('borneDemande', function (coords) {
+		borneDemande(io,coords[0],coords[1]);
+	})
+
 });
